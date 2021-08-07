7706c993911acbc4a7061359526a329669afd41a
Festus Agboma <festusagboma@gmail.com>
2021-08-07 02:23:20 +0100

    console.log

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const db = require('./config/db')
 const authRoutes = require('./route/auth')
 
 const app = express()
+const HOSTNAME = process.env.HOSTNAME || '0.0.0.0'
 const PORT = process.env.PORT || 5000
 
 /*await db.sync({
@@ -18,9 +19,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/api/auth', authRoutes)
 
-const server = app.listen(PORT, () => {
-    const host = server.address().address
-    console.log(host)
-
-    console.log(`Server started on port: /${PORT}`)
+const server = app.listen(PORT, HOSTNAME,() => {
+    console.log(`Server started on port: ${HOSTNAME}/${PORT}`)
 })
