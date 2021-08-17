f507496c1b235f4be3fcaa965e652b6af6c04b9c
Festus Agboma <festusagboma@gmail.com>
2021-08-17 06:12:21 +0100

    update

--- repo/post.js
+++ repo/post.js
@@ -29,7 +29,10 @@ module.exports = {
           include: {
             model: db.User,
             attributes: ['id', 'name']
-          }
+          },
+          order: [
+            ['createdAt', 'DESC']
+          ],
         }
       ]
     })
